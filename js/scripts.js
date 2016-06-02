3501df9c10f8234af4f3a70625f2e38e8e0a5026
Brett New and Stewart Cole <brett@>
2016-06-02 13:40:10 -0700

    determine your personal pet choise

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,92 @@
+
 $(document).ready(function() {
-  
+
+
+  $("#pet-choose").click(function() {
+    alert("hello");
+    var pet = parseInt($("#answer1").val()) + parseInt($("#answer2").val()) + parseInt($("#answer3").val()) + parseInt($("#answer4").val());
+    console.log(pet);
+    alert(pet);
+
+    debugger;
+    if (pet <= 2) {
+      $("#rock").show();
+      $("#turtle #snake").hide();
+      // $("#snake").hide();
+      $("#bunny").hide();
+      $("#hamham").hide();
+      $("#hedgehog").hide();
+      $("#duck").hide();
+      $("#puppy").hide();
+    } else if (2 < pet && pet <= 4) {
+      $("#rock").hide();
+      $("#turtle").show();
+      $("#snake").hide();
+      $("#bunny").hide();
+      $("#hamham").hide();
+      $("#hedgehog").hide();
+      $("#duck").hide();
+      $("#puppy").hide();
+    } else if (4 < pet && pet <= 6) {
+      $("#rock").hide();
+      $("#turtle").hide();
+      $("#snake").show();
+      $("#bunny").hide();
+      $("#hamham").hide();
+      $("#hedgehog").hide();
+      $("#duck").hide();
+      $("#puppy").hide();
+    } else if (6 < pet && pet <= 8) {
+      $("#rock").hide();
+      $("#turtle").hide();
+      $("#snake").hide();
+      $("#bunny").show();
+      $("#hamham").hide();
+      $("#hedgehog").hide();
+      $("#duck").hide();
+      $("#puppy").hide();
+    } else if (8 < pet && pet <= 10) {
+      $("#rock").hide();
+      $("#turtle").hide();
+      $("#snake").hide();
+      $("#bunny").hide();
+      $("#hamham").show();
+      $("#hedgehog").hide();
+      $("#duck").hide();
+      $("#puppy").hide();
+    } else if (10 < pet && pet <= 12) {
+      $("#rock").hide();
+      $("#turtle").hide();
+      $("#snake").hide();
+      $("#bunny").hide();
+      $("#hamham").hide();
+      $("#hedgehog").show();
+      $("#duck").hide();
+      $("#puppy").hide();
+    } else if (12 < pet && pet <= 14) {
+      $("#rock").hide();
+      $("#turtle").hide();
+      $("#snake").hide();
+      $("#bunny").hide();
+      $("#hamham").hide();
+      $("#hedgehog").hide();
+      $("#duck").show();
+      $("#puppy").hide();
+    } else if (14 < pet && pet <= 16) {
+      $("#rock").hide();
+      $("#turtle").hide();
+      $("#snake").hide();
+      $("#bunny").hide();
+      $("#hamham").hide();
+      $("#hedgehog").hide();
+      $("#duck").hide();
+      $("#puppy").show();
+    } else {
+      console.log("don't get a pet");
+    };
+  });
+
+
   $("#turtle").click(function() {
     $("#turtle p").toggle();
   });
@@ -8,8 +95,29 @@ $(document).ready(function() {
     $("#snake p").toggle();
   });
 
-  $("#caterpillar").click(function() {
-    $("#caterpillar p").toggle();
+  $("#rock").click(function() {
+    $("#rock p").toggle();
+  });
+
+  $("#bunny").click(function() {
+    $("#bunny p").toggle();
+  });
+
+  $("#duck").click(function() {
+    $("#duck p").toggle();
+  });
+
+
+  $("#hamham").click(function() {
+    $("#hamham p").toggle();
+  });
+
+  $("#hedgehog").click(function() {
+    $("#hedgehog p").toggle();
+  });
+
+  $("#puppy").click(function() {
+    $("#puppy p").toggle();
   });
 
 });
