d2853cbaff99f2e720c25892150a3be683cee4f8
Brett New and Stewart Cole <brett@>
2016-06-02 09:05:21 -0700

    It will create website with pictures of animals that display text when clicked on; used jquery.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  
+  $("#turtle").click(function() {
+    $("#turtle p").toggle();
+  });
+
+  $("#snake").click(function() {
+    $("#snake p").toggle();
+  });
+
+  $("#caterpillar").click(function() {
+    $("#caterpillar p").toggle();
+  });
+
+});
